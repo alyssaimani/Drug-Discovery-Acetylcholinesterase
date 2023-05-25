67519ad434d3389337b127b633ce64dd2e35f5b8
alyssaimani <56471157+alyssaimani@users.noreply.github.com>
2023-05-25 23:14:03 +0700

    update app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ def main():
     chem_class_model = torch.load('chemberta_classification.pth')
     chem_reg_model = torch.load('chemberta_regression.pth')
     
-    st.write('# Prediction Bioactivity of Alzheimer’s Drug Candidates using NLP based Approach')
-    st.write('The Process of drug discovery can take decades and cost billions of dollars until it is approved by FDA. Prediction of Molecular properties is one of a crucial task in drug discovery. Bioactivity values (e.g., IC50 values) are molecular properties which have commonly used to evaluate and select potential drug candidates. To increase the efficiency in the process of drug discovery, lots of research have been conducted by implementing machine learning to do drug candidate screening. In this study, we conduct NLP based approach by fine-tuning of Transformers Model on Alzheimer’s drug candidates to predict their bioactivity. We conduct fine-tuning on classification and regression tasks. For comparison we compare the result with QSAR random forest model and logistic regression.')
+    st.write('# Fine-Tuning Transformers Model for Prediction Bioactivity of Alzheimer’s Drug Candidates')
+    st.write('The lack of effectiveness of the wet lab approach to test all potential compounds as drug candidates, incur many in silico (computer-driven) approach has been conducted to reach the efficiency. Prediction of Molecular properties is one of a crucial task in drug discovery. Bioactivity values (e.g., IC50 values) are molecular properties which have commonly used to evaluate and select potential drug candidates. To increase the efficiency in the process of drug discovery, lots of research have been conducted by implementing machine learning to do drug candidate screening. Where, the implementation machine learning such as Random Forest and Extreme Gradient Boosting (XGBoost) can significantly reduce the number of in vitro test. Language model especially Transformers, recently became alternative to solve the challenge in drug discovery which also have been effectively used to generate molecular representation. This research investigate how well ChemBERTa as one of the Transformers models generate canonical SMILES representation and perform prediction on Alzheimer’s Drug Candidates dataset.')
     st.write('')
     st.write('## Fine-Tuning with ChemBERTa')
-    input_molecule = st.text_input('Input molecule to predict:',placeholder='example input CN(C(=O)n1nc(-c2ccc(Cl)cc2)nc1SCC(F)(F)F)c1ccccc1')
-    chem_class_result = chem_class_model.predict([input_molecule])
-    chem_reg_result = chem_reg_model.predict([input_molecule])
+    input_compound = st.text_input('Input molecule to predict:',placeholder='example input O=C(N1CCCCC1)n1nc(-c2ccc(Cl)cc2)nc1SCC(F)(F)F')
+    chem_class_result = chem_class_model.predict([input_compound])
+    chem_reg_result = chem_reg_model.predict([input_compound])
 
     chem_bioactivity_code = chem_class_result[0][0]
     chem_reg_score = str(chem_reg_result[0])
@@ -20,14 +20,16 @@ def main():
     chem_bioactivity_class = label_mapping[chem_bioactivity_code]
 
     col1, col2 = st.columns(2)
-    if input_molecule:
+    if input_compound:
         with col1:
-            st.write('### Classification Result')
+            st.write('### Bioactivity Class')
             st.write(chem_bioactivity_class)
 
         with col2:
-            st.write('### Regression Result')
-            st.write(chem_reg_score)
+            st.write('### pIC50 Value')
+            st.write(chem_reg_score, 'Molar')
 
+    st.write('## Note')
+    st.write('compound with IC50 value > 6 Molar indicated as an “active” compound and IC50 value < 5 Molar indicated as an “inactive” compound')
 if __name__ == '__main__':
     main()
